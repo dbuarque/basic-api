e4186c7e746c7a84a5568a121b67b461c3545c95
dbuarque <diogenes.buarque@gmail.com>
2016-02-24 14:29:27 -0300

    add models, routes and controllers

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var userSchema = new Schema({
+		"gender": String,
+		"name": {
+			"title": String,
+			"first": String,
+			"last": String
+		},
+		"location": {
+			"street": String,
+			"city": String,
+			"state": String,
+			"zip": Number
+		},
+		"email": String,
+		"username": String,
+		"password": String,
+		"salt": String,
+		"md5": String,
+		"sha1": String,
+		"sha256": String,
+		"registered": Number,
+		"dob": Number,
+		"phone": String,
+		"cell": String,
+		"PPS": String,
+		"picture": {
+			"large": String,
+			"medium": String,
+			"thumbnail": String
+		}
+});
+
+module.exports = mongoose.model('user', userSchema);
